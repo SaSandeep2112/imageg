2d898f44cd89e6d72826b0df1d5ed628d29c2a84
SaSandeep2112 <147911135+SaSandeep2112@users.noreply.github.com>
2024-06-08 12:53:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+// const app = require('./app.js');
+const express = require("express");
+const imageRouter = require('./routes/imageRouter.js');
+const authRouter = require("./routes/authRouter.js");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({origin: true}));
+app.use(express.json());
+
+app.use('/api/v1/auth', authRouter);
+
+app.use((req, res, next) => {
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        token = req.headers.authorization.split(' ')[1];
+    }
+    try{
+        jwt.verify(token, 'my-secret-123');
+        next();
+    }
+    catch(err){
+        return res.status(401).json({
+            status: 'fail',
+            message: 'Login required',
+        })
+    }
+})
+
+app.use('/api/v1/images', imageRouter);
+
+mongoose.connect("mongodb://127.0.0.1/Project").then(() => {
+  console.log('-------- Database Connected --------');
+});
+
+app.listen(1600,() => {
+    console.log('----------- App Started -----------')
+});
